d76c3f4d194a52f958698cfc41b360d7de570815
emilymstev <emilymstev@gmail.com>
2024-06-15 08:41:50 -0500

    modified:   wi-app/public/index.html 	modified:   wi-app/src/App.css 	modified:   wi-app/src/components/Navbar.css 	modified:   wi-app/src/components/Navbar.js

--- wi-app/src/components/Navbar.js
+++ wi-app/src/components/Navbar.js
@@ -1,43 +1,36 @@
 import React from "react";
+import { Component } from "react";
 import "./Navbar.css"
 import logo from "./filler-photos/WIM_logo_color.png";
 
-function Navbar() {
-    return (
-        <div class="nav">
+class Navbar extends Component {
+    state = { clicked: false };
+    handleClick = () => {
+        this.setState({clicked : !this.state.clicked})
+    }
+    render () {
+        return (
             <nav>
                 <a href="index.html">
                     <img src={logo} alt={"logo"}></img>
                 </a>
                 <div>
-                    <ul id="navbar">
+                    <ul id="navbar" className={this.state.clicked ? "#navbar active" : "#navbar"}>
                         <li><a className="active" href="index.html">Home</a></li>
                         <li><a href="index.html">Sustainability</a></li>
                         <li><a href="index.html">FAQ</a></li>
                         <li><a href="index.html">Contact</a></li>
                     </ul>
                 </div>
+                <div id="mobile" onClick= {this.handleClick}>
+                    <i id="bar"
+                    className={this.state.clicked ?
+                        "fas fa-times" : "fas fa-bars"
+                    }></i>
+                </div>
             </nav>
-        </div>
-
-        /**
-         * <Navbar expand = "lg" className = "bg-body-tertiary">
-            <Container>
-                <Navbar.Brand href="#home">
-                    <img src={logo} alt={"logo"} height="30"/>
-                </Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="me-auto">
-                        <Nav.Link href="#home">Home</Nav.Link>
-                        <Nav.Link href="#sustainability">Sustainability</Nav.Link>
-                    </Nav>
-                </Navbar.Collapse>
-            </Container>
-        </Navbar>
-         */
-        
-    )
+        );
+    }
 }
 
 export default Navbar;
