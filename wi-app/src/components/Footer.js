af372e789278bc3cca69cbbfc7e0602ab35af03d
emilymstev <emilymstev@gmail.com>
2024-06-22 17:49:11 -0400

    modified:   wi-app/src/App.css 	modified:   wi-app/src/components/Footer.css 	modified:   wi-app/src/components/Footer.js 	modified:   wi-app/src/components/Navbar.css

--- wi-app/src/components/Footer.js
+++ wi-app/src/components/Footer.js
@@ -6,34 +6,47 @@ import "./Footer.css";
 function Footer() {
     return (
         <div class="foot">
-            <img class="white-wi" src={footLogo} alt={"white wilmington iron logo"}></img>
+            <div class="content">
+                <div class = "white-wi">
+                    <img src={footLogo} alt={"white wilmington iron logo"}></img>
+                </div>
 
-            <div class="social">
-                <a class="linkedin" href="https://www.linkedin.com/company/wilmington-iron-&-metal-co.-inc" target="_blank" rel="noopener noreferrer">
-                    <img src={linkedInLogo} alt={"LinkedIn logo"}></img>
-                </a>
-                <p>(###)###-####</p>
-            </div>
+                <div class="social">
+                    <a class="linkedin" href="https://www.linkedin.com/company/wilmington-iron-&-metal-co.-inc" target="_blank" rel="noopener noreferrer">
+                        <img src={linkedInLogo} alt={"LinkedIn logo"}></img>
+                    </a>
+                    <a class="phone" href="tel:6303527340">(###)###-####</a>
+                    <a class = "email" href="malito:emilymstev@gmail.com">email@gmail.com</a>
+                </div>
 
-            <div class="hours">
-                <h4>Hours</h4>
-                <p>Monday: 00:00 - 00:00</p>
-                <p>Tuesday: 00:00 - 00:00</p>
-                <p>Wednesday: 00:00 - 00:00</p>
-                <p>Thursday: 00:00 - 00:00</p>
-                <p>Friday: 00:00 - 00:00</p>
-                <p>Saturday: CLOSED</p>
-                <p>Sunday: CLOSED</p>
-            </div>
-
-            <div class="location">
-                <h4>Location</h4>
-                <a class="google-map" href="https://www.google.com/maps" target="_blank" rel="noopener noreferrer">
+                <div class="hours">
+                    <h4>Hours</h4>
                     <ul>
-                        <li>99 S 9 St.</li>
-                        <li>Nine, IA 50423</li>
+                        <li>Monday: 00:00 - 00:00</li>
+                        <li>Tuesday: 00:00 - 00:00</li>
+                        <li>Wednesday: 00:00 - 00:00</li>
+                        <li>Thursday: 00:00 - 00:00</li>
+                        <li>Friday: 00:00 - 00:00</li>
+                        <li>Saturday: CLOSED</li>
+                        <li>Sunday: CLOSED</li>
                     </ul>
-                </a>
+                </div>
+
+                <div class="location">
+                    <h4>Location</h4>
+                    <a class="google-map" href="https://maps.app.goo.gl/Zn3kJPiNNfNXmbS98" target="_blank" rel="noopener noreferrer">
+                        <ul>
+                            <li>2149 S. South St,</li>
+                            <li>Wilmington, OH 45177</li>
+                        </ul>
+                    </a>
+                </div>
+            </div>
+            
+
+            <div class="copyright">
+                <hr/>
+                <h6>Copyright &copy; {(new Date().getFullYear())}. All Rights Reserved.</h6>
             </div>
         </div>
     );
